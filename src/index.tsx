4cdf9e12b42882c2ee6eaaa2e64c8fc16cd02f34
Khizri Makhmudov <khizrim@khizrim.ru>
2023-04-08 11:05:53 +0300

    feat: configure linters

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
-createRoot(document.getElementById("root") as HTMLElement).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <h1>Hello, World</h1>
   </React.StrictMode>
