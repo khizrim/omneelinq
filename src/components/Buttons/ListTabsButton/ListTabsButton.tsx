8135bf92839ad2dd0902bb66bb643df7241668fe
Khizri Makhmudov <khizrim@khizrim.ru>
2023-10-21 20:29:12 +0300

    chore: uses better label for tab parser button

--- src/components/Buttons/ListTabsButton/ListTabsButton.tsx
+++ src/components/Buttons/ListTabsButton/ListTabsButton.tsx
@@ -3,15 +3,15 @@ import React from 'react';
 import { ArrowDownFromLine } from '@gravity-ui/icons';
 
 import { Button, type ButtonProps } from 'src/components/Button';
-import { PARSE_TAB_URLS_BUTTON } from 'src/utils/constants';
+import { LIST_TABS_URLS_BUTTON } from 'src/utils/constants';
 
-export const ParseTabsButton = ({ onClick, ...props }: ButtonProps) => (
+export const ListTabsButton = ({ onClick, ...props }: ButtonProps) => (
   <Button
     icon={ArrowDownFromLine}
     onClick={onClick}
-    tooltip={PARSE_TAB_URLS_BUTTON.tooltip}
+    tooltip={LIST_TABS_URLS_BUTTON.tooltip}
     {...props}
   >
-    {PARSE_TAB_URLS_BUTTON.label}
+    {LIST_TABS_URLS_BUTTON.label}
   </Button>
 );
