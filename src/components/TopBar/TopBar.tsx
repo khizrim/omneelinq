eddddc0d85178318affa4586b1d07fcaccc0505c
Khizri Makhmudov <khizrim@khizrim.ru>
2023-10-21 20:59:38 +0300

    fix: adds small padding after topbar

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import { Flex, Label } from '@gravity-ui/uikit';
+import { Flex, Label, spacing } from "@gravity-ui/uikit";
 
 import { BugReport } from 'src/components/BugReport';
 import { Logo } from 'src/components/Logo';
 import { EXTENSION_VERSION } from 'src/utils/constants';
 
 export const TopBar = () => (
-  <Flex justifyContent={'space-between'} alignItems={'center'} gap={4}>
+  <Flex className={spacing({pb: 4})} justifyContent={'space-between'} alignItems={'center'} gap={4}>
     <Logo />
     <Label>{EXTENSION_VERSION}</Label>
     <BugReport />
