b0c78bbe28689f4cb4d465a0dfc05ad54c844314
Khizri Makhmudov <khizrim@khizrim.ru>
2023-10-29 18:01:07 +0300

    fix: prettier issues

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
-import { Flex, Label, spacing } from "@gravity-ui/uikit";
+import { Flex, Label, spacing } from '@gravity-ui/uikit';
 
 import { BugReport } from 'src/components/BugReport';
 import { Logo } from 'src/components/Logo';
 import { EXTENSION_VERSION } from 'src/utils/constants';
 
 export const TopBar = () => (
-  <Flex className={spacing({pb: 4})} justifyContent={'space-between'} alignItems={'center'} gap={4}>
+  <Flex
+    className={spacing({ pb: 4 })}
+    justifyContent={'space-between'}
+    alignItems={'center'}
+    gap={4}
+  >
     <Logo />
     <Label>{EXTENSION_VERSION}</Label>
     <BugReport />
