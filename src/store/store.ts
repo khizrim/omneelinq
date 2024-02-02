0e520a8739badbebb6702344cceee107336ce0cc
Khizri Makhmudov <khizrim@khizrim.ru>
2024-02-02 20:30:04 +0300

    chore: remove unused type definition

--- src/store/store.ts
+++ src/store/store.ts
@@ -7,9 +7,6 @@ import {
   LOCAL_STORAGE_SORT_KEY,
   LOCAL_STORAGE_URLS_KEY,
 } from 'src/utils/constants';
-import type { LocalStorageKey } from 'src/utils/types';
-
-export type SettingsProps = Record<'settings', Record<LocalStorageKey, string>>;
 
 const INITIAL_VALUE = {
   [LOCAL_STORAGE_LAZY_LOAD_KEY]: 'off',
